0cfa7cdd12245a07178e1441466c541642decb50
jaleelio89 <132376263+jaleelio89@users.noreply.github.com>
2023-09-03 16:13:27 -0500

    Add files via upload

--- connection.php
+++ connection.php
@@ -1,12 +1,20 @@
-<?php      
-    $host = "localhost";  
-    $user = "root";  
-    $password = '';  
-    $db_name = "javatpoint";  
-      
-    $con = mysqli_connect($host, $user, $password, $db_name);  
-    if(mysqli_connect_errno()) {  
-        die("Failed to connect with MySQL: ". mysqli_connect_error());  
-    }  
-?>  \\\\\\\\\\\\\\\\\\\\\\\?>
+<?php
+	$Username = $_POST['Username'];
+	$Email Address = $_POST['email'];
+	$Password = $_POST['password'];
+	$Confirm_password = $_POST['Confirm_password'];
 
+$conn = new mysqli('localhost','root','','signup');
+	if($conn->connect_error){
+		echo "$conn->connect_error";
+		die("Connection Failed : ". $conn->connect_error);
+	} else {
+		$stmt = $conn->prepare("insert into registration(Username, email, password,Confirm_password ) values(?, ?, ?, ?,)");
+		$stmt->bind_param("ssss", $Username, $Email Address, $Password, $Confirm password);
+		$execval = $stmt->execute();
+		echo $execval;
+		echo "Registration successfully...";
+		$stmt->close();
+		$conn->close();
+		}
+?>
